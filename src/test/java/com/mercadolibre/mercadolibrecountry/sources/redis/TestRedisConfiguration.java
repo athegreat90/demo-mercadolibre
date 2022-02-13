b7dc0aefcda2f5545ee123f3fe165e2ee99f8913
athegreat90 <alexander.thgrt@live.de>
2022-02-13 13:42:25 -0500

    Update TestRedisConfiguration.java

--- src/test/java/com/mercadolibre/mercadolibrecountry/sources/redis/TestRedisConfiguration.java
+++ src/test/java/com/mercadolibre/mercadolibrecountry/sources/redis/TestRedisConfiguration.java
@@ -1,11 +1,11 @@
 package com.mercadolibre.mercadolibrecountry.sources.redis;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.TestConfiguration;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Random;
 
 @TestConfiguration
 public class TestRedisConfiguration
@@ -13,16 +13,24 @@ public class TestRedisConfiguration
     private RedisServer redisServer;
 
     public TestRedisConfiguration() {
-        this.redisServer = RedisServer.builder().port(6379).setting("maxheap 2gb").build();
+        this.redisServer = RedisServer.builder().port(new Random().nextInt(7000)).setting("maxheap 2gb").setting("timeout 30000").build();
     }
 
     @PostConstruct
     public void postConstruct() {
+        if (redisServer.isActive())
+        {
+            return;
+        }
         redisServer.start();
     }
 
     @PreDestroy
     public void preDestroy() {
+        if (!redisServer.isActive())
+        {
+            return;
+        }
         redisServer.stop();
     }
 }
